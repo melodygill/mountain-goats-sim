7b1a8ecedb07d61f9c129e4c448b3cc66df4db3b
Melody Gill <melodywgill@gmail.com>
2024-07-14 21:53:33 -0700

    Add game_controller.py

---
+++ src/game_controller.py
@@ -0,0 +1,18 @@
+class GameController:
+    """
+    Initialize players and game state
+    """
+    def __init__(self):
+        pass
+
+    def validate_move(self, move):
+        pass
+
+    """
+    Update game state to reflect that the move was made
+    """
+    def implement_move(self, move):
+        pass
+
+    def requery(self):
+        pass
